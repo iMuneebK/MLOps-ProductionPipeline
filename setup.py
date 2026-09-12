c0f292a0e17aff70320e77dc10e4f82817a03d4a
Muhammad Muneeb Khan <153149641+iMuneebK@users.noreply.github.com>
2026-09-13 01:33:00 +0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlops_pipeline',
+    version='0.0.1',
+    author='smune',
+    packages=find_packages(),
+    install_requires=[],
+)
